7f1b83b26f98c72ffaf2fee63f50282ecde507c0
Filip Ignjatic <56887898+filipignjatic@users.noreply.github.com>
2021-08-08 21:31:42 +0200

    Update sort_alg.vhd

--- sort_alg.vhd
+++ sort_alg.vhd
@@ -25,16 +25,16 @@ end sort_alg;
 architecture rtl of sort_alg is
   -- Memory
   type data_array is array (0 to 1023) of std_logic_vector(15 downto 0); -- max number of transactions is 1024
-  signal data, sort_data : data_array := (others => (others => '0'));
+  signal data : data_array := (others => (others => '0'));
    -- States for FSM
-  type state_type is (idle,drive_tready, collect_done, transfer, increment_t, assign, compare, check, increment, drive_tvalid,  drive_tdata); -- need to be edited
+  type state_type is (idle,drive_tready, collect_done, assign_first, assign_second, compare, swap_first_addr, swap_first_data, swap_second_addr, swap_second_data, check, increment, drive_tvalid,  drive_tdata);
   signal curr_state, next_state: state_type;
   -- Local signals
-  signal i, j, d, x: integer := 0; -- Increment integers
-  signal count: integer range 0 to 1023; -- field for number of transactions
+  signal i, j: integer := 0; -- Increment integers
+  signal counter, addr: integer range 0 to 1023; -- field for number of transactions
   signal tmp_i, tmp_j, mem_data: std_logic_vector(15 downto 0); -- Signals for temp values of data(i) and data(i+1)
   -- Local signals for driving data
-  signal write_en, drive_done: std_logic;
+  signal write_en, read_en, start_counter,  drive_done: std_logic;
    
   begin
   
@@ -53,27 +53,38 @@ architecture rtl of sort_alg is
     begin
         if(rising_edge(clk)) then
             -- collecting data
-            if(write_en = '1' and ain_tlast = '0') then
-                data(count) <= mem_data;
-                count <= count + 1;
-                drive_done <= '0';
-            end if;
+            if(write_en = '1') then
+                data(addr) <= mem_data;
             -- driving data
-            if(write_en = '0' and aout_tready = '1') then
-                   if(d /= count-1) then
-                       aout_tdata <= data(d);
-                       d <= d + 1;
-                       drive_done <= '0';
-                   else
-                        aout_tdata <= data(d);
-                        drive_done <= '1';
-                   end if; 
+            elsif(read_en = '1') then
+                aout_tdata <= data(counter);
             end if;
         end if;
     end process;
     
+    COUNT: process(clk) is 
+    begin
+        if(rising_edge(clk)) then
+            if(start_counter = '1') then
+                counter <= counter + 1;
+            end if;
+            
+            if(ain_tlast = '1') then -- Block for decrement counter for one, makeing it right
+                counter <= counter - 1;
+            end if;
+            
+            if(read_en = '1') then
+                if(counter /= 0) then
+                    counter <= counter -1;
+                else
+                    drive_done <= '1';
+                end if;
+            end if;
+        end if;
+    end process;
+      
     
-     process(curr_state, ain_tdata, ain_tlast, ain_tvalid, drive_done) is
+    FSM:process(curr_state, ain_tlast, ain_tvalid, drive_done, ain_tdata) is
     begin
     case curr_state is
         when idle =>
@@ -81,68 +92,83 @@ architecture rtl of sort_alg is
             aout_tvalid <= '0';
             tmp_i <= (others => '0');
             tmp_j <= (others => '0');
-            sort_data <= (others => (others => '0'));
+            read_en <= '0';
+            write_en <= '0';
             if(ain_tvalid = '1') then
                 next_state <= drive_tready;
             end if;
         when drive_tready =>
-                ain_tready <= '1';
-                next_state <= collecting;
-        when collecting =>
             if(ain_tlast = '1') then
+                mem_data <= ain_tdata;
+                start_counter <= '0';
                 next_state <= collect_done;
             else
-              mem_data <= ain_tdata;
-              write_en <= '1';
-              next_state <= collecting;
+                write_en <= '1';
+                ain_tready <= '1';
+                mem_data <= ain_tdata;
+                addr <= counter;
+                start_counter <= '1';
+                next_state <= drive_tready;
+            end if;
         when collect_done =>
                 ain_tready <= '0';
                 write_en <= '0';
                 next_state <= assign_first;
         when assign_first =>
-           tmp_i <= sort_data(i);
+           tmp_i <= data(i);
            next_state <= assign_second;
         when assign_second =>
-           tmp_j <= sort_data(i+1);
+           tmp_j <= data(i+1);
+           next_state <= compare;
         when compare =>
-            if(tmp_i > tmp_j) then
-              swap <= '1';
-                sort_data(i+1) <= tmp_i;
-                sort_data(i) <= tmp_j;
-                next_state <= swap_temp_i;
+            if(tmp_i < tmp_j) then
+                next_state <= swap_first_addr;
             else
-               swap <= '0';
                 next_state <= increment;
             end if;
-        when swap_temp_i =>
-             mem_state <= temp_i;
-             next_state <= swap_temp_j;
-        when swap_temp_j =>
-             mem_state <= temp_j;
+        when swap_first_addr =>
+            addr <= i+1;
+            next_state <= swap_first_data;
+        when swap_first_data =>
+            mem_data <= tmp_i;
+            write_en <= '1';
+            next_state <= swap_second_addr;
+        when swap_second_addr =>
+            addr <= i;
+            write_en <= '0';
+            next_state <= swap_second_data;
+        when swap_second_data =>
+            mem_data <= tmp_j;
+            write_en <= '1';
+            next_state <= increment;
         when increment =>
-            if(j /= count-2) then
+            write_en <= '0';
+            if(j /= counter-2) then
                 i <= i+1;
                 next_state <= check;
             else
                 next_state <= drive_tvalid;
             end if;
-            if(i = count-2) then
+            if(i = counter-2) then
                 j <= j+1;
             end if;
         when check =>
-                if(i = count-1) then
+                if(i = counter-1) then
                     i <= 0;
-                    next_state <= assign;
+                    next_state <= assign_first;
                 else
-                    next_state <= assign;
+                    next_state <= assign_first;
                 end if;
         when drive_tvalid =>
             aout_tvalid <= '1';
             next_state <= drive_tdata;
         when drive_tdata =>
+            read_en <= '1';
             if(drive_done = '1') then
-                next_state <= idle;
                 aout_tlast <= '1';
+                next_state <= idle;
+            else
+                next_state <= drive_tdata;
             end if;
     end case;
     end process;
