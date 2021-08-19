dabc9fe68437b0a876250ac41ccfcd0439020daa
Filip Ignjatic <56887898+filipignjatic@users.noreply.github.com>
2021-08-19 20:14:56 +0200

    Update master_driver.sv

--- verif/sv/master_driver.sv
+++ verif/sv/master_driver.sv
@@ -80,7 +80,7 @@ task master_driver::process_item(trans_item item);
   // Driving data
   m_vif.ain_tvalid  = 1'b1;
   @(posedge m_vif.ain_tready);
-  for(int i = 1; i < item.num_data; i ++ ) begin
+  for(int i = 0; i < item.num_data; i ++ ) begin
     m_vif.ain_tdata = $urandom_range(1, 500);
     if(i == item.num_data - 1) begin
       m_vif.ain_tlast = 1'b1;
